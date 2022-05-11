e11a0373fcafc02dc3879e19f122332d9583f518
Alexander Morales Panitz <alexander.morales@utec.edu.pe>
2022-05-11 11:55:03 -0500

    <fix> Fixed sorted alphabetical and counter

--- hw1/rabbit.c
+++ hw1/rabbit.c
@@ -5,78 +5,86 @@
  */
 
 #include "rabbit.h"
+void init_rabbits(rabbit_list_t *wclist) { list_init(wclist); }
 
-void init_rabbits(rabbit_list_t* wclist) { list_init(wclist); }
+rabbit_t *add_rabbit(rabbit_list_t *wclist, char *word)
+{
+  rabbit_t *rabbit = find_rabbit(wclist, word);
 
-rabbit_t* add_rabbit(rabbit_list_t* wclist, char* word) {
-  rabbit_t* rabbit = find_rabbit(wclist, word);
-
-  if (rabbit == NULL) {
-    rabbit_t* rabbit = (rabbit_t*) malloc(sizeof(rabbit_t));
-    rabbit->breed = malloc(strlen(word)+1);
-    strncpy(rabbit->breed, word, sizeof(word));
+  if (rabbit == NULL)
+  {
+    rabbit = (rabbit_t *)malloc(sizeof(rabbit_t));
+    rabbit->breed = malloc(strlen(word) + 1);
+    strncpy(rabbit->breed, word, strlen(word) + 1);
     rabbit->count = 1;
     list_push_back(wclist, &rabbit->elem);
-  } else {
+  }
+  else
+  {
     rabbit->count++;
   }
   return rabbit;
 }
 
-rabbit_t* find_rabbit(rabbit_list_t* wclist, char* word) { 
+rabbit_t *find_rabbit(rabbit_list_t *wclist, char *word)
+{
   bool empty_list = list_empty(wclist);
-  if (empty_list) return NULL;
+  if (empty_list)
+    return NULL;
   struct list_elem *e;
-  rabbit_t* temp;
-  for (e = list_begin(wclist); e != list_end(wclist); e = list_next(e)) {
+  rabbit_t *temp;
+  for (e = list_begin(wclist); e != list_end(wclist); e = list_next(e))
+  {
     temp = list_entry(e, rabbit_t, elem);
-    if (strcmp(temp->breed, word) == 0) {
+    if (strcmp(temp->breed, word) == 0)
+    {
       return temp;
     }
   }
   return NULL;
 }
 
-size_t total_rabbits(rabbit_list_t* wclist) {
+size_t total_rabbits(rabbit_list_t *wclist)
+{
   int count = 0;
-  struct rabbit* r;
-  struct list_elem* e;
-  for (e = list_begin(wclist); e != list_end(wclist); e = list_next(e)) {
-     r = list_entry(e, rabbit_t, elem);
+  struct rabbit *r;
+  struct list_elem *e;
+  for (e = list_begin(wclist); e != list_end(wclist); e = list_next(e))
+  {
+    r = list_entry(e, rabbit_t, elem);
     count += r->count;
-    
   }
   return count;
 }
 
-size_t total_breeds(rabbit_list_t* rlist) {
-  return list_size(rlist);
-}
+size_t total_breeds(rabbit_list_t *rlist) { return list_size(rlist); }
 
-
-
-
-void fprint_rabbits(rabbit_list_t* wclist, FILE* outfile) {
-  struct list_elem* e;
-  for (e = list_begin(wclist); e < list_end(wclist); e = list_next(e)) {
-    struct rabbit* r = list_entry(e, rabbit_t, elem);
-    outfile = fopen("./output_rabbit.txt", "w");
-    if (outfile == NULL) {
+void fprint_rabbits(rabbit_list_t *wclist, FILE *outfile)
+{
+  struct list_elem *e;
+  struct rabbit *r;
+  for (e = list_begin(wclist); e != list_end(wclist); e = list_next(e))
+  {
+    r = list_entry(e, rabbit_t, elem);
+    if (outfile == NULL)
+    {
       printf("Error!");
       exit(1);
     }
-    fprintf(outfile, "%s: %d\n", r->breed, r->count);
-   }
-   fclose(outfile);
+    fprintf(outfile, "%s: %i\n", r->breed, r->count);
+  }
 }
 
-static bool less_list(const struct list_elem* ewc1,
-                      const struct list_elem* ewc2, void* aux) {
-  /* TODO */
-  return false;
+
+static bool less_list(const struct list_elem *ewc1,
+                      const struct list_elem *ewc2, void *aux)
+{
+  bool(*aux_ptr)(const rabbit_t *, const rabbit_t *) = aux;
+  return (*aux_ptr)(list_entry(ewc1, rabbit_t, elem),list_entry(ewc2, rabbit_t, elem));
 }
 
-void rabbit_sort(rabbit_list_t* wclist,
-                 bool less(const rabbit_t*, const rabbit_t*)) {
+void rabbit_sort(rabbit_list_t *wclist,
+                 bool less(const rabbit_t *, const rabbit_t *))
+{
   list_sort(wclist, less_list, less);
 }
--- hw1/rabbit_helper.c
+++ hw1/rabbit_helper.c
@@ -4,15 +4,16 @@
  * You may modify this file, and are expected to modify it.
  */
 
-
 #include "rabbit_helper.h"
 
 bool less_count(const rabbit_t* r1, const rabbit_t* r2) {
-    /* TODO */
-    return false;
+  if (r1->count == r2->count) {
+    return (r1->breed[0] < r2->breed[0]);
+  }
+  return (r1->count < r2->count);
 };
 
 bool less_word(const rabbit_t* r1, const rabbit_t* r2) {
-    /* TODO */
+  if (r1->breed[0] < r2->breed[0]) return true;
   return false;
 };
